878db6e7752c31129ea8e02958528aee71a1ed08
mmr689 <miguelmolinarotger@gmail.com>
2024-02-28 10:40:29 +0100

    model

---
+++ test.py
@@ -0,0 +1,5 @@
+import tflite_runtime.interpreter as tflite
+
+model_path = r'models\best_yolov3n_int8.tflite'
+interpreter = tflite.Interpreter(model_path,
+  experimental_delegates=[tflite.load_delegate('libedgetpu.so.1')])
