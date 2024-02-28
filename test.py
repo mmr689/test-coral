3353c34576900749156e7081513af9b45cb4323d
mmr689 <miguelmolinarotger@gmail.com>
2024-02-28 11:17:57 +0100

    TFL

--- test.py
+++ test.py
@@ -3,3 +3,9 @@ import tflite_runtime.interpreter as tflite
 model_path = 'models/best_yolov3n_int8.tflite'
 interpreter = tflite.Interpreter(model_path,
   experimental_delegates=[tflite.load_delegate('libedgetpu.so.1')])
+print('A1')
+
+interpreter.allocate_tensors()
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
+print('A2')
